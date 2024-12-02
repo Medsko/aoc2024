a67a3989a1f601b5076804bd9a576a062029e717
medsko <medsko@posteo.net>
2024-12-02 11:55:06 +0100

    Expand script for initializing day

--- src/init_day.sh
+++ src/init_day.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
 SRC_DIR=/home/medsko/IdeaProjects/aoc/aoc2024/src/
-MAIN_KT_DIR="${SRC_DIR}main/kotlin"
-TEST_DIR="${SRC_DIR}test/kotlin"
+MAIN_KT_DIR="${SRC_DIR}main/kotlin/nl/medsko"
+TEST_DIR="${SRC_DIR}test/kotlin/nl/medsko"
 INPUT_DIR="${SRC_DIR}test/resources"
 
 # Check if at least one argument is passed
@@ -26,12 +26,44 @@ if [[ $DAY -lt 10 ]]; then
   DAY_STRING="0${DAY}"
 fi
 
+echo "package nl.medsko\n\nclass Day${DAY_STRING} {\n" > "$MAIN_KT_DIR/Day${DAY_STRING}.kt"
+echo "    fun partOne(input: List<String>): Int {\n        return 1\n    }\n" >> "$MAIN_KT_DIR/Day${DAY_STRING}.kt"
+echo "    fun partTwo(input: List<String>): Int {\n        return 1\n    }\n" >> "$MAIN_KT_DIR/Day${DAY_STRING}.kt"
+echo "}" >> "$MAIN_KT_DIR/Day${DAY_STRING}.kt"
 
-echo "class Day${DAY_STRING} {\n}" > "$MAIN_KT_DIR/Day${DAY_STRING}.kt"
 touch "$INPUT_DIR/day${DAY_STRING}.txt"
 touch "$INPUT_DIR/example/day${DAY_STRING}.txt"
 
-#touch "$TEST_DIR/Day${DAY_STRING}Test.kt"
-echo "import org.junit.jupiter.api.Test\n\nclass Day${DAY_STRING}Test {\n\n    @Test\n    fun \`Example part 1\`() {}\n\n    @Test\n    fun \`Answer part 1\`() {}\n\n    @Test\n    fun \`Example part 2\`() {}\n\n    @Test\n    fun \`Answer part 2\`() {}\n}" > "$TEST_DIR/Day${DAY_STRING}Test.kt"
+# TODO: import util function (after move to package
+echo "package nl.medsko\n\nimport org.junit.jupiter.api.Assertions.assertEquals\nimport org.junit.jupiter.api.Test\n\nclass Day${DAY_STRING}Test {\n" > "$TEST_DIR/Day${DAY_STRING}Test.kt"
+echo "    private val subject = Day${DAY_STRING}()\n" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+
+echo "    @Test\n    fun \`Example part 1\`() {" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+echo "        val answer = subject.partOne(getTestInputForDay(${DAY}))" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+echo "        println(\"Example answer to part one: \$answer\")" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+echo "        assertEquals(0, answer)" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+echo "    }\n\n" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+
+echo "    @Test\n    fun \`Answer part 1\`() {" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+echo "        val answer = subject.partOne(getRealInputForDay(${DAY}))" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+echo "        println(\"Answer to part one: \$answer\")" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+echo "        assertEquals(0, answer)" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+echo "    }\n" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+
+echo "    @Test\n    fun \`Example part 2\`() {" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+echo "        val answer = subject.partTwo(getTestInputForDay(${DAY}))" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+echo "        println(\"Example answer to part two: \$answer\")" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+echo "        assertEquals(0, answer)" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+echo "    }\n\n" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+
+echo "    @Test\n    fun \`Answer part 2\`() {" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+echo "        val answer = subject.partTwo(getRealInputForDay(${DAY}))" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+echo "        println(\"Answer to part two: \$answer\")" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+echo "        assertEquals(0, answer)" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+echo "    }\n" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+
+
+echo "}" >> "$TEST_DIR/Day${DAY_STRING}Test.kt"
+
 
 # TODO: add to git
